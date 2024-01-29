cfca4e58c669391644da597c950f1cf231b1a192
Ben Xiang <jonben3215@engr2-23-184-dhcp.int.colorado.edu>
2024-01-29 11:35:46 -0700

    Updated Player Test

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
     @Test
@@ -12,7 +11,50 @@ public class PlayerTest {
 
        Player player1 = new Player(Name, health);
 
-       assertNotNull(Name);
-       assertNotNull(player1.getName());
+       assertNotNull(player1);
     }
+
+    @Test
+    public void Test_getname()
+    {
+        String Name  = "Ben";
+        int health = 10;
+
+        Player player1 = new Player(Name, health);
+        assertNotNull(player1.getName());
+    }
+    @Test
+    public void Test_getHealth()
+    {
+        String Name  = "Ben";
+        int health = 10;
+
+        Player player1 = new Player(Name, health);
+       assertEquals(health, player1.getHealth());
+    }
+    @Test
+    public void Test_setName()
+    {
+        String Name  = "Ben";
+        String newName = "William";
+        int health = 10;
+
+        Player player1 = new Player(Name, health);
+        player1.setName(newName);
+
+        assertEquals(newName, player1.getName());
+    }
+
+    @Test
+    public void Test_setHealth()
+    {
+        String Name  = "Ben";
+        int health = 10;
+        int new_Health = 8;
+
+        Player player1 = new Player(Name, health);
+        player1.setHealth(new_Health);
+        assertEquals(new_Health, player1.getHealth());
+    }
+
 }

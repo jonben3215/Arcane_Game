ca537127420c35b2d254e0cff4e69ae8b5bc3c9e
Daniel Hatakeyama <daha4242@colorado.edu>
2024-01-31 19:45:17 -0700

    the room class has basic functionality but lots is left open ended due to architecture uncertainties

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Room {
+
+
+    // TODO eventually rework direction to be decoupled and behind the scenes (no need to directly modify or set)
+    // TODO it should be calculated automatically based on position of room.
+    public enum Direction {
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST
+    }
+
     // Static Variables:
     private static String DESCRIPTION_FILE_NAME; // Put default descriptionFileName here
 
@@ -7,40 +21,68 @@ public class Room {
 
     // ---------- Member Variables: ---------- //
     private int ID = -1;
-    private String name = "Default Name";
-    private String description = "Default Description";
+    private String name = "Default";
+    private String description = "Default";
 
+    // TODO make more expandable and general for future updates
+    // TODO ideally neighborID list only, and dynamically calculate all directions
+    Map<Direction, Integer> neighbors = new HashMap<>();
     // ---------- Constructors: ---------- //
 
     // Default Constructor
     public Room() {
         generateID();
+        generateDescription();
+        resetNeighbors();
     }
 
+    // Constructor
     public Room(String name) {
-        // All constructors must call generateID(), TODO make this better later
-        generateID();
+        // All constructors implement setup, this is done using 'this()' TODO make less shitty
+        this();
         this.name = name;
     }
 
-
     // ---------- Public Methods ---------- //
-    public void addRoom(String direction, int roomID) {
 
+    public void addNeighbor(Direction d, int roomID) {
+        neighbors.put(d, roomID);
+    }
+
+    public void removeNeighbor(Direction d) {
+        neighbors.put(d, null);
+    }
+
+    // TODO once there is better neighbor setup, (or a need for it) remove neighbor with roomID
+    public void removeNeighbor(int roomID) {
+        // TODO not implemented yet, waiting to see how bad or good the implementation of the maze room system is
+        // neighbors.put(d, roomID);
+        return;
     }
 
     // ---------- Helpers ---------- //
 
     // TODO make this function get a random description from the description file
-    private String generateDescription() {
-        return "This is the default description";
+    private void generateDescription() {
+        this.description = "This is the default description";
     }
 
     private void generateID() {
         this.ID = ID_TRACKER ++;
     }
 
+    private void resetNeighbors() {
+        this.neighbors.clear();
+        for (Direction d : Direction.values()) {
+            this.neighbors.put(d, null);
+        }
+    }
+
     // ---------- Getters / Setters ---------- //
+
+    public Map<Direction, Integer> getNeighbors() {
+        return this.neighbors;
+    }
 
     public String getName() {
         return this.name;
@@ -53,6 +95,10 @@ public class Room {
 
     public int getID() {
         return this.ID;
+    }
+
+    public String getDescription() {
+        return this.description;
     }
 
     /**

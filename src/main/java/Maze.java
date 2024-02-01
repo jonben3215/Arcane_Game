1096d6186d3b19616a0322d8b7ad9f24c49a45a2
Daniel Hatakeyama <daha4242@colorado.edu>
2024-01-31 18:34:10 -0700

    Added shit to room class and room test, but i cant run it on my machine idk wtf is going on

--- src/main/java/Maze.java
+++ src/main/java/Maze.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+
 public class Maze {
-    private int num_rooms;
-    private ArrayList<rooms> rooms;
+    private int num_rooms = 0;
+    private ArrayList<Room> rooms;
 
     public void generate() {
         // Generate rooms with neighbors
     }
 
+    // Default Constructor
+    public Maze () {
+    }
 }
--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,19 +1,87 @@
-public class Room
-{
-    private String name;
-    private String description;
 
-    public void addRoom (String direction, int roomID)
+public class Room {
+    // Static Variables:
+    private static String DESCRIPTION_FILE_NAME; // Put default descriptionFileName here
 
-    private static String description_text_file_name;
+    private static int ID_TRACKER = 0;
 
-    public Room(String name)
-    {
-        this.Name = name;
+    // ---------- Member Variables: ---------- //
+    private int ID = -1;
+    private String name = "Default Name";
+    private String description = "Default Description";
+
+    // ---------- Constructors: ---------- //
+
+    // Default Constructor
+    public Room() {
+        generateID();
     }
 
-    public String getName()
-    {
-        return this.Name;
+    public Room(String name) {
+        // All constructors must call generateID(), TODO make this better later
+        generateID();
+        this.name = name;
     }
+
+
+    // ---------- Public Methods ---------- //
+    public void addRoom(String direction, int roomID) {
+
+    }
+
+    // ---------- Helpers ---------- //
+
+    // TODO make this function get a random description from the description file
+    private String generateDescription() {
+        return "This is the default description";
+    }
+
+    private void generateID() {
+        this.ID = ID_TRACKER ++;
+    }
+
+    // ---------- Getters / Setters ---------- //
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        if (name == null) return;
+        this.name = name;
+    }
+
+    public int getID() {
+        return this.ID;
+    }
+
+    /**
+     * Get descriptionTextFile
+     * WARNING: Will return 'null' if fileName is undefined
+     * (fileName is undefined by default, assign it with Room.setDescriptionTextFile)
+     */
+    public static String getDescriptionFileName() {
+        return DESCRIPTION_FILE_NAME;
+    }
+
+    /**
+     * Updates filePath for descriptionTextFile.
+     * Will not update unless file name is valid
+     * descriptionTextFile is where rooms source their description texts from
+     *
+     * @param filePath A valid file path
+     */
+    public static void setDescriptionFileName(String filePath) {
+        // Do not update if null filePath
+        if (filePath == null || filePath.isEmpty()) return;
+
+        // TODO validate file path more thoroughly, validate file extension, etc.
+
+        // If filePath passes checks, update the filePath
+        DESCRIPTION_FILE_NAME = filePath;
+    }
+
+    // TODO automatically open + handle file instance with static methods, called on program run and filePath update
+
 }
+

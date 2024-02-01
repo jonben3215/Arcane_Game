3e89fa5285619789cd41f6d901594a007fffa11b
Daniel Hatakeyama <daha4242@colorado.edu>
2024-01-31 18:34:10 -0700

    Added shit to room class and room test, but i cant run it on my machine idk wtf is going on

---
+++ src/test/java/MazeTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MazeTest {
+    @Test
+    public void DefaultMazeCreation() {
+        Maze maze = new Maze();
+        assertNotNull(maze);
+
+        // TODO Check Default Values
+    }
+
+}

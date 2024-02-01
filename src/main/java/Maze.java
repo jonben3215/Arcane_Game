f5dac9421d85786aee0afd9ce22fceef0421d301
Daniel Hatakeyama <daha4242@colorado.edu>
2024-01-31 20:45:49 -0700

    Maze class works to generate simple rectangle maze of rooms. There is no global position, only links to neighbors. Right now uses a custom id system, but may be better to have references to Room objects. All of the neighbor relations are set up for a n by m dimension rectangle.

--- src/main/java/Maze.java
+++ src/main/java/Maze.java
@@ -2,10 +2,59 @@ import java.util.ArrayList;
 
 public class Maze {
     private int num_rooms = 0;
-    private ArrayList<Room> rooms;
+    private ArrayList<Room> rooms = new ArrayList<>();
 
-    public void generate() {
-        // Generate rooms with neighbors
+    private void ConnectRooms(Room roomA, Room roomB) {
+
+    }
+    private void connectRooms(int roomA, int roomB) {
+
+    }
+
+    public void GenerateRectangleMaze(int rows, int cols) {
+
+        // Rectangle grid approach to generate maze.
+        rooms.clear();
+
+        // Make list of length * width
+        for (int i = 0; i<(rows*cols); i++) {
+            rooms.add(new Room());
+        }
+
+        // Update num_rooms
+        num_rooms = rooms.size();
+
+        // Get neighbors of rooms in rectangle array:
+        for (int room_idx = 0; room_idx < rooms.size(); room_idx++) {
+
+            // Get relevant data
+            Room room = rooms.get(room_idx);
+            int north_idx = room_idx - cols;
+            int south_idx = room_idx + cols;
+            int east_idx = room_idx + 1;
+            int west_idx = room_idx - 1;
+
+            // Add neighbor if valid
+            if (north_idx >= 0) room.addNeighbor(Room.Direction.NORTH, (rooms.get(north_idx)).getID());
+            if (south_idx <= num_rooms -1) room.addNeighbor(Room.Direction.SOUTH, (rooms.get(south_idx)).getID());
+            if (room_idx%cols != 0) room.addNeighbor(Room.Direction.WEST, (rooms.get(west_idx)).getID());
+            if (room_idx%cols != cols-1) room.addNeighbor(Room.Direction.EAST, (rooms.get(east_idx)).getID());
+
+        }
+
+    }
+
+    public Room getRoomByID (int ID) {
+        for (Room room : this.rooms) {
+            if (room.getID() == ID) {
+                return room;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Room> getRooms () {
+        return this.rooms;
     }
 
     // Default Constructor
--- src/test/java/MazeTest.java
+++ src/test/java/MazeTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MazeTest {
@@ -9,6 +11,37 @@ public class MazeTest {
         assertNotNull(maze);
 
         // TODO Check Default Values
+    }
+
+    @Test
+    public void RectangleMazeCreation() {
+        Maze maze = new Maze();
+        assertNotNull(maze);
+
+        // TODO Check Default Values
+
+        int rows = 10;
+        int cols = 20;
+
+        maze.GenerateRectangleMaze(rows,cols);
+
+        assertNull(maze.getRoomByID(-1));
+
+        ArrayList<Room> rectRoomArray = maze.getRooms();
+
+        Room nwCorner = rectRoomArray.get(0);
+        Room neCorner = rectRoomArray.get(cols-1);
+        Room swCorner = rectRoomArray.get(rectRoomArray.size()-cols);
+        Room seCorner = rectRoomArray.get(rectRoomArray.size()-1);
+
+        System.out.println(nwCorner.neighbors);
+        System.out.println(neCorner.neighbors);
+        System.out.println(swCorner.neighbors);
+        System.out.println(seCorner.neighbors);
+
+        // assertEquals(nwCorner.neighbors, 0);
+
+
     }
 
 }

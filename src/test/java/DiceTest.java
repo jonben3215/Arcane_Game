18bf06892d49bc5e914eca6a1dcd0cdd6f1aa3f7
Ben Xiang <benxiang3215@gmail.com>
2024-02-10 22:01:15 -0700

    Updated Map and checking if Id exist

--- src/test/java/DiceTest.java
+++ src/test/java/DiceTest.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class DiceTest {
+import org.junit.jupiter.api.Test;
+
+public class DiceTest {
+
+    @Test
+    public void Test_DiceRoll()
+    {
+        Dice die = new Dice();
+        int number = die.roll();
+
+        System.out.print(number);
+
+        //Making sure the Generated number is within 1 and 6
+        if(number >= 1 && number <= 6)
+            assert true;
+        else
+            assert false;
+    }
+
 }

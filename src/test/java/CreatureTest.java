1f54d86d0b67586e5eb40f9c82a7c71d33ea6d2a
Ben Xiang <benxiang3215@gmail.com>
2024-02-10 22:01:15 -0700

    Updated Map and checking if Id exist

--- src/test/java/CreatureTest.java
+++ src/test/java/CreatureTest.java
@@ -1,14 +1,13 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CreatureTest {
 
     @Test
     public void CreatureCreation()
     {
-        Creature Creature_1 = new Creature("Goblin");
+        Creature Creature_1 = new Creature("Goblin", 10);
         assertNotNull(Creature_1);
     }
 
@@ -16,8 +15,24 @@ public class CreatureTest {
 
     public void Test_getName()
     {
-        Creature Creature_1 = new Creature("Goblin");
+
+        Creature Creature_1 = new Creature("Goblin", 10);
         assertEquals("Goblin", Creature_1.getName());
+    }
+
+    @Test
+    public void test_isAlive()
+    {
+        String Name  = "Ben";
+        int health = 0;
+        // int health = 1;
+        int new_Health = 8;
+
+        Creature creature1 = new Creature (Name, health);
+        //assertTrue(player1.isAlive());
+
+        assertFalse(creature1.isAlive());
+
     }
 
 }

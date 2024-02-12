04c93bd58ea8201a9a766e05bfacfce2c6e83dbd
Ben Xiang <benxiang3215@gmail.com>
2024-02-12 14:23:40 -0700

    Hw3

--- src/main/java/Creature.java
+++ src/main/java/Creature.java
@@ -1,9 +1,12 @@
-import java.util.Vector;
+import java.util.Random;
 
 public class Creature {
     private String CreatureName;
     private int creatureHealth;
-    private int pos[];//Position for the crature on the map
+    private int[] pos;//Position for the crature on the map
+
+    private int roomIdx = 0;
+    int number_creature = 5;
     private double health;
 
     // On constructor, set random starting postion, name, health=5
@@ -12,6 +15,7 @@ public class Creature {
     {
         this.CreatureName = Cname;
         this.creatureHealth = Chealth;
+        pos = new int[number_creature];
     }
     public String getName()
     {
@@ -32,8 +36,54 @@ public class Creature {
         return creatureHealth > 0;
     }
 
-    public void creatureMovement(int x, int y)
+    public int Creature_Roll()
     {
+        Random numGenerator = new Random();
 
+        //Generates a number from 1 to 6 similar to a die
+        return numGenerator.nextInt(6)+1;
     }
+
+    private boolean isRoomIdxUsed(int newRoomIdx) {
+        for (int existingRoomIdx : pos) {
+            if (existingRoomIdx == newRoomIdx) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public void spawnCreature(Maze maze)
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < number_creature; i++)
+        {
+            int newRoomIdx;
+            do {
+                newRoomIdx = random.nextInt(9) + 1;
+            } while (isRoomIdxUsed(newRoomIdx)); // Check if the new room index is already in pos array
+
+            this.pos[i] = newRoomIdx;
+        }
+    }
+
+//    public void displayCreaturePosition()
+//    {
+//        for (int po : pos) System.out.println(po);
+//    }
+
+    public boolean checkIfCreature(int newRoomIdx) {
+        for (int existingRoomIdx : pos) {
+            if (existingRoomIdx == newRoomIdx) {
+                System.out.println("Creature Found");
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
 }

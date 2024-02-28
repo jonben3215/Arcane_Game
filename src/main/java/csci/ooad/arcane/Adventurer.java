349d2cbe19344f73f679a6cfe47199587de91ed8
Daniel Hatakeyama <daha4242@colorado.edu>
2024-02-27 17:26:40 -0700

    want to commit but cann't xdxdxd

--- src/main/java/csci/ooad/arcane/Adventurer.java
+++ src/main/java/csci/ooad/arcane/Adventurer.java
@@ -220,6 +220,6 @@ public class Adventurer extends Agent{
         this.room.removeFood(foodToEat);
         this.setHealth(this.health+1.0);
 
-        logger.info("csci.ooad.arcane.Adventurer " + this.getInfo() + " just ate " + foodToEat.getName());
+        logger.info("Adventurer " + this.getInfo() + " just ate " + foodToEat.getName());
     }
 }
